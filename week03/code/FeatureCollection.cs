06cb7aebceb6f958e70154e2b9160dd88bc083ff
wardt12 <105118740+wardt12@users.noreply.github.com>
2024-06-28 16:03:21 -0600

    Update FeatureCollection.cs

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,4 +1,18 @@
-public class FeatureCollection {
+public class FeatureCollection
+{
+    public List<Feature> Features { get; set; }
+}
+
+public class Feature
+{
+    public Properties Properties { get; set; }
+}
+
+public class Properties
+{
+    public string Place { get; set; }
+    public float Mag { get; set; }
+}
+
     // Todo Earthquake Problem - ADD YOUR CODE HERE
     // Create additional classes as necessary
-}
